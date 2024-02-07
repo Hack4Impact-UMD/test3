dfd9339dd5f8a0fd9ca24fd68a83c16a256f4338
Sahil Gaba <sgaba9778@gmail.com>
2024-02-06 20:29:14 -0500

    found

---
+++ react-app/src/App.js
@@ -0,0 +1,16 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/first" element={<h1>Hi</h1>} />
+        <Route path="/sec" element={<h1>Hi2</h1>} />
+        <Route path="*" element={<h1>Hi Nt Found</h1>} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
